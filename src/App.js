db567f451e2d0574fadc2e88fb1229cd10dcf91e
Jones Ogolo <jay.topher11@gmail.com>
2020-01-11 09:49:14 +0100

    mounted searchbar and logs on the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useEffect } from "react";
+import SearchBar from "./components/layout/SearchBar";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import Logs from "./components/logs/Logs";
 
 function App() {
   useEffect(() => {
     // initialize materialize js
     M.AutoInit();
   });
-  return <div className="App">My App</div>;
+  return (
+    <>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </>
+  );
 }
 
 export default App;
