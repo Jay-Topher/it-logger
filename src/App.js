470c2658b80d7e2a5ebdbebac99fa09ede5d8859
Jones Ogolo <jay.topher11@gmail.com>
2020-01-11 18:36:26 +0100

    mounted the components created so far

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 function App() {
   useEffect(() => {
@@ -15,6 +20,11 @@ function App() {
     <>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </>
