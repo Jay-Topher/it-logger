8066865beff8dbaf334707e9bf57007dcc569579
Jones Ogolo <jay.topher11@gmail.com>
2020-01-14 20:31:21 +0100

    created tech reducer

---
+++ src/reducers/techReducer.js
@@ -0,0 +1,17 @@
+import {} from '../actions/types'
+
+const initialState = {
+  techs: null,
+  loading: false,
+  error: null
+}
+export default (state = initialState, action) => {
+  switch (action.type) {
+    // case value:
+      
+    //   break;
+  
+    default:
+      return state;
+  }
+}
